f700ca737a2e01f591a967d7ee25ac735a0287a0
Wes-Isaac <yisehakmitiku@gmail.com>
2022-12-05 19:18:48 +0800

    Seed database

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validate :title, presence :true, length:{minimum: 4, maximum: 50}
-  validate :description, presence :true, length: {minimum:15, maximum: 250}
+  validates :title, presence: true, length:{minimum: 4, maximum: 50}
+  validates :description, presence: true, length:{minimum:15, maximum: 250}
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,3 @@
 class Search < ApplicationRecord
-  validate :query, presence :true, length:{minimum: 4, maximum,50}
+  validates :query, presence: true, length:{minimum: 4, maximum,50}
 end
