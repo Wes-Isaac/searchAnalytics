275dd2c367cc240318e75d2260e9471f1368681c
Wes-Isaac <yisehakmitiku@gmail.com>
2022-12-06 20:31:00 +0800

    Fix typo

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,7 @@ class Search < ApplicationRecord
 
   def isPresent?(key)
     jarow = FuzzyStringMatch::JaroWinkler.create(:native)
-    similarity_percentage = jarow.getDistance(search, query)
+    similarity_percentage = jarow.getDistance(key, query)
     similarity_percentage > 0.8
   end
 end
