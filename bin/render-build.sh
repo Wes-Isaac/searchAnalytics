b633ba16971ffb44288db0d6460d813f16ff265d
Isaac <yisehakmitiku@gmail.com>
2022-12-09 10:23:36 +0300

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,9 @@
 set -o errexit
 
 bundle install
+bundle exec rails stimulus:manifest:update
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
-bundle exec rails stimulus:manifest:update
+
